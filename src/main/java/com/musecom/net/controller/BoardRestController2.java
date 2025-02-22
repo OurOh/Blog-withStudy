d06b3cc1d7eb48f9c72a3cc466087dc4409ef800
OurOh <ahn980321@gmail.com>
2025-02-22 17:51:19 +0900

    0222 BoardRestController

--- src/main/java/com/musecom/net/controller/BoardRestController2.java
+++ src/main/java/com/musecom/net/controller/BoardRestController2.java
@@ -5,6 +5,7 @@ import com.musecom.net.dto.Board2;
 import com.musecom.net.service.BoardService;
 import com.musecom.net.service.BoardService2;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,13 +43,27 @@ public class BoardRestController2 {
         }
     }
 
-    //Update
-    @PutMapping("id");
-    public
+    @PutMapping("/{id}")
+    public ResponseEntity<Integer> updateBoard(@PathVariable Long id, @RequestBody Board2 board2){
+        board2.setId(id);
+        Board2 result = boardService2.updateBoard(board2);
+        if (result != null) {
+            return ResponseEntity.ok(1);
+        } else {
+            return ResponseEntity.internalServerError().body(0);
+        }
+    }
 
-
-    //Delete
-    @DeleteMapping("id");
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Integer> deleteBoard(@PathVariable Long id) {
+        Board2 result = boardService2.selectBoard(id);
+        if (result != null) {
+            boardService2.deleteBoard(id);
+            return ResponseEntity.ok(1);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 
 }
